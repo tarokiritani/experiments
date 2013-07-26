532dbaca761a6e7fff835ccdab52541a29414ea4
Taro Kiritani <taro.kiritani@epfl.ch>
2013-07-26 15:59:10 +0200

    added surgery to chronic cranial window.

--- app/models/mouse.rb
+++ app/models/mouse.rb
@@ -4,6 +4,6 @@ class Mouse < ActiveRecord::Base
   has_many :cells, dependent: :destroy
   belongs_to :user
   
-  SPECIES_OPTIONS = ["mouse-c57/bl6", "tdTomato", "Gad2-ires-Cre/Rosa-CreR-tdTomato", "etv1-cre", "PV-Cre/ROSA-Cre-tdTomato"].sort
+  SPECIES_OPTIONS = ["mouse-c57/bl6", "tdTomato", "Gad2-ires-Cre/Rosa-CreR-tdTomato", "B6(Cg)-Etv1<tm1.1(cre/ERT2)Zjh>/J", "B6(Cg)-Etv1<tm1.1(cre/ERT2)Zjh>/J/ROSA-CreRtomato", "PV-Cre/ROSA-Cre-tdTomato"].sort
   validates_inclusion_of :species_strain, :in => SPECIES_OPTIONS
 end
--- app/models/surgery.rb
+++ app/models/surgery.rb
@@ -2,7 +2,7 @@ class Surgery < ActiveRecord::Base
   attr_accessible :amount_in_ul, :surgery_type, :location, :operation_date, :titer, :drug_application, :weight, :comment
   belongs_to :mouse
   
-  SURGERY_OPTIONS = ["lumafluor red", "lumafluor blue", "CTB488", "implantation", "craniotomy", "euthanasia", "tamoxifen injection (100mg/kg)", "cardiac perfusion"].sort  
+  SURGERY_OPTIONS = ["lumafluor red", "lumafluor blue", "CTB488", "implantation", "craniotomy", "euthanasia", "tamoxifen injection (100mg/kg)", "cardiac perfusion", "chronic cranial window"].sort  
   validates_inclusion_of :surgery_type, :in => SURGERY_OPTIONS
   
 end
